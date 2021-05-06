03aef68bfa2724690b1aa97ba28d42078792b659
RyoKitatani <ryo0929m@gmail.com>
2021-05-07 01:26:55 +0900

    Initial Commit

---
+++ HelloApp/HelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppApp.swift
+//  HelloApp
+//
+//  Created by RyoKitatani on 2021/05/07.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
